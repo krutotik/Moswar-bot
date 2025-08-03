f330ea750fda357f48311bd7bb9876515e65cf99
Daniil St <danillnew1@gmail.com>
2025-08-03 18:54:55 +0200

    added option not to turn off browser after script is executed

--- configuration/configuration.py
+++ configuration/configuration.py
@@ -12,16 +12,17 @@ def set_options():
     Returns:
         Options: A configured Options object with the specified settings.
     """
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
+
     options = Options()
+    options.add_argument(f"user-agent={user_agent}")
     options.add_argument("start-maximized")
     options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
     # options.add_argument("--ignore-certificate-errors")
     # options.add_argument("--ignore-ssl-errors")
     # options.add_argument("--disable-logging")
     options.add_argument("--log-level=3")
-
-    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
-    options.add_argument(f"user-agent={user_agent}")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option("detach", True)
 
     return options
