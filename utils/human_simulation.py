8821206938de6e81e19685042cc7faa15592ff54
Daniil St <danillnew1@gmail.com>
2025-08-09 14:04:43 +0200

    updated default parameters for random_delay function to enhance delay range

--- utils/human_simulation.py
+++ utils/human_simulation.py
@@ -3,15 +3,15 @@ import time
 
 
 # Function to introduce a small random delay
-def random_delay(min_time: float = 2, max_time: float = 3) -> None:
+def random_delay(min_time: float = 3, max_time: float = 4) -> None:
     """
     Pause execution for a random duration within a specified range.
 
     This function introduces a delay to simulate human-like behavior.
 
     Parameters:
-        min_time (float): The minimum delay time in seconds. Default is 2 seconds.
-        max_time (float): The maximum delay time in seconds. Default is 3 seconds.
+        min_time (float): The minimum delay time in seconds. Default is 3 seconds.
+        max_time (float): The maximum delay time in seconds. Default is 4 seconds.
 
     Example:
         random_delay(1, 3)  # Pauses execution for a random duration between 1 and 3 seconds.
